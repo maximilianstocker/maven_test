f5dfbe5042856e92482b9380a50e5b3a5d50ade2
Maximilian Stocker <iwanttodonate@googlemail.com>
2019-08-24 15:02:30 +0200

    gui test 1

--- src/main/java/TestPlugin.java
+++ src/main/java/TestPlugin.java
@@ -1,2 +1,34 @@
-public class TestPlugin {
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+public class TestPlugin implements PlugInFilter {
+
+    ImagePlus imagePlus = new ImagePlus();
+
+    @Override
+    public int setup(String arg, ImagePlus imp) {
+        imagePlus.setTitle("Test Plugin");
+        showGenericDialog();
+
+        return 0;
+    }
+
+    @Override
+    public void run(ImageProcessor ip) {
+    }
+
+    private void showGenericDialog() {
+        try {
+            GenericDialog dialog = new GenericDialog("Stack Selection");
+            dialog.addMessage("New Message.");
+
+            if (dialog.wasOKed()) {
+                System.out.println("ok");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/test/java/TestPluginTest.java
+++ src/test/java/TestPluginTest.java
@@ -1,11 +1,18 @@
+import ij.ImagePlus;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestPluginTest {
 
     @Test
     void setup() {
+        ImagePlus testImage = new ImagePlus();
+        testImage.setTitle("Test Plugin");
+
+        TestPlugin plugin = new TestPlugin();
+        plugin.setup("", testImage);
+        assertEquals(testImage.getTitle(), plugin.imagePlus.getTitle());
     }
 
     @Test
