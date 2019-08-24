748ec3e7f68f4dbbe523bfeae517d36fe10b123b
Maximilian Stocker <iwanttodonate@googlemail.com>
2019-08-24 14:19:10 +0200

    Initial commit

---
+++ src/test/java/TestPluginTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestPluginTest {
+
+    @Test
+    void setup() {
+    }
+
+    @Test
+    void run() {
+    }
+}
